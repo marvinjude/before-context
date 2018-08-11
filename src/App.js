6b87f4a5e0eb0779b7f7af60f5c971872f50100f
Marvin Jude <marvinjudehk@gmail.com>
2018-08-12 10:34:49 +1300

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react'; 
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 import NG from './NG.png';
 
-// Just a styled div for the <App> component
+// Media template sizes
+const sizes = {
+  large: 1200,
+  medium: 992,
+  small: 768,
+  extraSmall: 766
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {});
+
 const AppWrapper = styled.div`
-   background-color: #360c2a;;
-   height:100vh;
-   display: flex;
-   justify-content:center;
-   align-items:center;
-   flex-direction:column;
+  background-color:black;
+  height:100vh;
 `
-const NavbarWrapper = styled.div`
-   background-color:white;
-   border-radius: 20px;
-   box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
-   position:relative;
+const CenteredFlex = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+`
+const NavbarWrapper = CenteredFlex.extend`
+   background-color:black; 
+   height:8vh;
+   justify-content: flex-end;
 `
 const UserName = styled.div`
   text-align:center;
-  font-size:30px;
+  font-size:25px;
   text-transform: uppercase;
   color:#636060;
+`
+const PageLayoutWrapper = styled.div`
+  height:100vh;
 `
 const UserPosition = styled.div`
   text-transform: uppercase;
@@ -29,17 +49,37 @@ const UserPosition = styled.div`
   color: #8d907e;
   margin:5px;
 `
-const UserInfo = styled.div`
+const UserInfo = CenteredFlex.extend`
+   background-color:white;
    padding:20px;
-   display: flex;
-   justify-content:center;
-   align-items:center;
    flex-direction:column;
 `
-const ImageIcon = styled.img.attrs(`
-  src : ${props=> props.src}
-`)`
-  width:50px;
+const MainWrapper = CenteredFlex.extend`
+  background-color:white;
+  position:relative;
+  height:92vh;
+`
+const Image = styled.img.attrs(`
+  src : ${props => props.src}
+`)
+`
+  width:100%;
+  ${props => props.rounded && css`
+     border-radius:50%;
+  `}
+  ${props => props.small && css`
+     width:40px;
+     height:40px;
+  `}
+`
+
+const Card = styled.div`
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
+  border-radius:20px;
+  
+  ${media.medium`width:50%`}
+  ${media.small`width:50%`}
+  ${media.extraSmall`width:80%`}
 `
 
 class App extends Component {
@@ -58,38 +98,51 @@ class App extends Component {
   render() {
     return (
       <AppWrapper>
-        <PageLayout navbar={<NavigationBar user={this.state.user}/>}/>
+        <PageLayout>
+          <NavigationBar avatar={<Avatar user={this.state.user} />} />
+          <Main user={this.state.user}/>
+        </PageLayout>
       </AppWrapper>
     );
   }
 }
 
-const PageLayout = ({navbar}) => {
+const PageLayout = ({children}) => {
   return (
-    <React.Fragment>
-      {navbar}
-    </React.Fragment>
+    <PageLayoutWrapper>
+      {children}
+    </PageLayoutWrapper>
   );
 }
 
-const NavigationBar = ({user}) => {
+const NavigationBar = ({avatar}) => {
   return (
     <NavbarWrapper>
-      <Avatar user={user} />
-      <UserInfo>
-        <UserName>{user.name}</UserName>
-        <UserPosition>
-          {user.position}
-        </UserPosition>
-        <ImageIcon src={NG}/>
-      </UserInfo>
+      {avatar}
     </NavbarWrapper>
   );  
 }
 
+
+
+const Main = ({ user }) => { 
+  return (
+    <MainWrapper>
+      <Card>
+        <Image src={user.image} />
+        <UserInfo>
+          <UserName>{user.name}</UserName>
+          <UserPosition>{user.position}</UserPosition>
+          <Image small src={NG}/>
+        </UserInfo>
+      </Card>
+    </MainWrapper>
+  )
+}
+
 const Avatar = ({ user }) => {
   return (
-     <img src={user.image}/>
+     <Image small rounded src={user.image} />
   )
 }
 
